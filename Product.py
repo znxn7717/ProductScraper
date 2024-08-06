3ef5b626a5c2643b359a37b74e00cc1a3ac92dc5
znxn7717 <81914668+znxn7717@users.noreply.github.com>
2024-08-07 03:06:52 +0330

    sql12.freemysqlhosting.net

--- Product.py
+++ Product.py
@@ -7,7 +7,7 @@ import json
 class Database:
     def __init__(self):
         load_dotenv()
-        self.host = os.getenv('DB_HOST', 'http://sql12.freemysqlhosting.net/')
+        self.host = os.getenv('DB_HOST', 'sql12.freemysqlhosting.net')
         self.port = os.getenv('DB_PORT', 3306)
         self.database = os.getenv('DB_DATABASE', 'sql12724427')
         self.user = os.getenv('DB_USER', 'sql12724427')
